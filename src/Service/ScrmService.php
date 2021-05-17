922937a0220584787b145ef8d33c52289a0af9d8
bigabiga_bellorchid <zhongyunchang@yeah.net>
2021-05-17 16:15:09 +0800

    feat: #76635 增加SCRM相关业务

--- src/Service/ScrmService.php
+++ src/Service/ScrmService.php
@@ -4,8 +4,19 @@ namespace ESCloud\SDK\Service;
 
 class ScrmService extends BaseService
 {
-    protected $host = '';
+    protected $host = 'scrm-service.qiqiquyu.net';
 
     protected $service = 'scrm';
+
+
+    public function getUserByToken($token)
+    {
+         return $this->request('GET',  '/api/console/customer/byToken', array('token' => $token));
+    }
+
+    public function verifyOrder($token)
+    {
+        return $this->request('POST', '', array());
+    }
 
 }
